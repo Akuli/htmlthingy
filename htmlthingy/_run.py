1995c7120d94f4eedc7ff03137f07ba2ec86eeb6
Akuli <akuviljanen17@gmail.com>
2017-10-22 15:06:50 +0300

    put sidebar inside content

--- htmlthingy/_run.py
+++ htmlthingy/_run.py
@@ -61,11 +61,11 @@ class Builder:
 
                 file.write('<body>\n')
 
+                file.write('<div id="content">\n')
                 sidebar = self.get_sidebar_content(txtfile)
                 if sidebar is not None:
                     file.write('<div id="sidebar">%s</div>\n' % sidebar)
 
-                file.write('<div id="content">\n')
                 for chunk in self.converter.convert(content, txtfile):
                     file.write(chunk)
                 file.write('</div>\n')
